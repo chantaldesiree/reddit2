ae301ac60f28ebb1cf12d54fa802da8305f11027
chantalwiebe <chantal.wiebe@gmail.com>
2020-07-09 09:46:59 -0500

    First commit

---
+++ src/components/FrontPage/FrontPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Post from '../Post/Post'
+
+function FrontPage() {
+    return (
+      <div className="postContainer">
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+      </div>
+    );
+  }
+
+  export default FrontPage;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+      <>
+        <ul>
+            <li className="logo">Logo</li>
+            <li>Create a Post</li>
+            <li>Messages</li>
+            <li>username12345678</li>
+        </ul>
+      </>
+    );
+  }
+
+  export default Navbar;
