c2193da6bbd9892e47bef88a968aa220244d8acd
chantaldesiree <chantaldesiree@outlook.com>
2020-07-16 17:31:17 -0500

    Fixed socialMedia alignment

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -9,50 +9,105 @@ function Post() {
     <>
       {PostInfo.map((post) => (
         <div className="post" key={post.postID}>
-          <div className="timestamp">
-            <strong>[{post.timestamp}]</strong>
+          <div className="postTitle">
+            <strong className="highlightColor title">{post.postTitle}</strong>
+            <p style={{ "margin-top": "0px" }}>
+              by <u>{post.author}</u> - <strong>[{post.timestamp}]</strong>
+            </p>
           </div>
 
-          <div className="postTitle">
-            <strong>{post.postTitle}</strong> by <u>{post.author}</u>
+          <div className="postText">
+            <p>{post.postText}</p>
           </div>
 
           <div className="socialMedia">
-            <p className="highlightColor">
+            <div>
               <a href="#">
-                <strong>{post.socialMedia.comments} </strong>
+                <strong className="highlightColor">
+                  {post.socialMedia.comments}{" "}
+                </strong>
                 <i className="fas fa-comment-dots" title="Comment"></i>
               </a>
-            </p>
-            <p className="highlightColor">
+            </div>
+            <div>
               <a href="#">
-                <strong>{post.socialMedia.likes} </strong>
+                <strong className="highlightColor">
+                  {post.socialMedia.likes}{" "}
+                </strong>
                 <i className="fas fa-heart" title="Like"></i>
               </a>
-            </p>
-            <p>
+            </div>
+            <div>
               <a href="#">
                 <i className="fas fa-share" title="Share"></i>
               </a>
-            </p>
-            <p>
+            </div>
+
+            <div>
               <a href="#">
                 <i className="fas fa-check-circle" title="Save"></i>
               </a>
-            </p>
-            <p>
+            </div>
+
+            <div>
               <a href="#">
                 <i className="fas fa-times-circle" title="Hide"></i>
               </a>
-            </p>
-          </div>
-          <div className="postText">
-            <p>{post.postText}</p>
+            </div>
           </div>
         </div>
       ))}
     </>
   );
 }
+
+// return (
+//   <>
+//     {PostInfo.map((post) => (
+//       <div className="post" key={post.postID}>
+//         <div className="timestamp">
+//           <strong>[{post.timestamp}]</strong>
+//         </div>
+
+//         <div className="postTitle">
+//           <strong>{post.postTitle}</strong> by <u>{post.author}</u>
+//         </div>
+
+//         <div className="socialMedia">
+//           <p className="highlightColor">
+//             <a href="#">
+//               <strong>{post.socialMedia.comments} </strong>
+//               <i className="fas fa-comment-dots" title="Comment"></i>
+//             </a>
+//           </p>
+//           <p className="highlightColor">
+//             <a href="#">
+//               <strong>{post.socialMedia.likes} </strong>
+//               <i className="fas fa-heart" title="Like"></i>
+//             </a>
+//           </p>
+//           <p>
+//             <a href="#">
+//               <i className="fas fa-share" title="Share"></i>
+//             </a>
+//           </p>
+//           <p>
+//             <a href="#">
+//               <i className="fas fa-check-circle" title="Save"></i>
+//             </a>
+//           </p>
+//           <p>
+//             <a href="#">
+//               <i className="fas fa-times-circle" title="Hide"></i>
+//             </a>
+//           </p>
+//         </div>
+//         <div className="postText">
+//           <p>{post.postText}</p>
+//         </div>
+//       </div>
+//     ))}
+//   </>
+// );
 
 export default Post;
